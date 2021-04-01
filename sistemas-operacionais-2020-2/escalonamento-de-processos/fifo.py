b4521c5999e28b41f017df2554ee45ef2d442131
Vinicius Finger <51798742+viniciusfinger@users.noreply.github.com>
2021-03-31 22:14:31 -0300

    .

--- sistemas-operacionais-2020-2/escalonamento-de-processos/fifo.py
+++
@@ -1,9 +0,0 @@
-from commons.processaArquivo import processaArquivo
-
-#Escalonamento FIRST IN - FIRST OUT
-
-arquivo = open("commons/processos.txt", 'r')
-processos = processaArquivo(arquivo)
-
-for processo in processos:
-    processo.executa()
