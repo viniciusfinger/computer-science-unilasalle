51f00cea3c2fe55000452a6217bdba2f75f93303
Vinicius Finger <51798742+viniciusfinger@users.noreply.github.com>
2021-08-25 08:46:45 -0300

    Melhoria na classe de prova de conceito

--- orientacao-objetos-2021-2/Trabalho aula 3/ProvaConceito.py
+++ orientacao-objetos-2021-2/Trabalho aula 3/ProvaConceito.py
@@ -1,7 +1,19 @@
 from Livro import Livro
 from Editora import Editora
 
-editora = Editora(1, "Editora Test", "Vinicius", "98304-3447")
+editoraTest = Editora(1, "Editora Test", "Vinicius", "983043447")
+editoraSegunda = Editora(2, "Editora Segunda", "André", "999999999")
 
-livro = Livro(1, "Um conto de terror baseado em fatos reais", 1342423, editora)
 
+livroConto = Livro(1, "Um conto de terror baseado em fatos reais", 1342423, editoraTest)
+livroTecnico = Livro(2, "Arquitetura Limpa: uma introdução a arquitetura de software", 109444, editoraSegunda)
+
+print("Livro de conto: ")
+print("Id: {}, Código ISBN: {}, Editora: {}, Descrição: {}".format(livroConto.codigo, livroConto.codigoISBN ,livroConto.editora.razaoSocial, livroConto.descricao))
+print("Contato {}(Cód.: {}):".format(livroConto.editora.razaoSocial, livroConto.editora.codigo))
+print("Responsável: {}, Fone: {}".format(livroConto.editora.nomeContato, livroConto.editora.telefone))
+print("------------------------")
+print("Livro técnico:")
+print("Id: {}, Código ISBN: {}, Editora: {}, Descrição: {}".format(livroTecnico.codigo, livroTecnico.codigoISBN, livroTecnico.editora.razaoSocial, livroTecnico.descricao))
+print("Contato {}(Cód.: {}):".format(livroTecnico.editora.razaoSocial, livroTecnico.editora.codigo))
+print("Responsável: {}, Fone: {}".format(livroTecnico.editora.nomeContato, livroTecnico.editora.telefone))
